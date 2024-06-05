a44de220ca335696636113c79cc69cd5c654dac9
GuilhermeMarques1 <g-marques@outlook.com>
2024-06-04 21:25:49 -0300

    Atividade 10 - Exercício 4

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,40 +1,50 @@
 export function Home() {
   return (
-    <>
-      <style>
-        {
-          `
-            .div {
-              width: 250px;
-              height: 250px;
-              margin-top: 15px;
-              background-color: burlywood;
-            }
-    
-            .div2 {
-              background: linear-gradient(to right, #ff8a00, #e52e71);
-              font-family: 'Comic Sans MS', cursive;
-              font-size: 30px;
-              color: white; 
-              text-align: center;
-              line-height: 100px;
-            }
-    
-            .div1{
-              border-radius: 20px;
-              background-image: linear-gradient(90deg, #000 50%, transparent 50%);
-              background-size: 4px 100%;
-            }
-    
-            .div1:hover {
-              transform: translateX(85vw );
-            }
-          `
-        }
-      </style>
-      <div className="div div1" ></div>
-      <div className="div div2" > Texto em comic sans</div>
-    </>
+    <div class="container">
+      <h2 class="mt-4">Tabela de Itens</h2>
+      <div class="table-responsive">
+        <table class="table table-striped mt-3">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Tipo</th>
+              <th>Preço</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+            <tbody>
+              <tr>
+                <td>001</td>
+                <td>Item A</td>
+                <td>Tipo A</td>
+                <td>$10.00</td>
+                <td>
+                  <button type="button" class="btn btn-primary btn-sm">Detalhes</button>
+                </td>
+              </tr>
+              <tr>
+                <td>002</td>
+                <td>Item B</td>
+                <td>Tipo B</td>
+                <td>$15.00</td>
+                <td>
+                    <button type="button" class="btn btn-primary btn-sm">Detalhes</button>
+                </td>
+              </tr>
+              <tr>
+                <td>003</td>
+                <td>Item C</td>
+                <td>Tipo C</td>
+                <td>$20.00</td>
+                <td>
+                  <button type="button" class="btn btn-primary btn-sm">Detalhes</button>
+                </td>
+              </tr>
+            </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
